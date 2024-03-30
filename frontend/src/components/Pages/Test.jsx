8db62305e9eeede41044e55b5374b1c3a11e1456
Voropaev Ivan <iavoropaev@edu.hse.ru>
2024-03-30 16:45:19 +0300

    Change px to rem

--- frontend/src/components/Pages/Test.jsx
+++ frontend/src/components/Pages/Test.jsx
@@ -2,8 +2,9 @@ import "./Test.css";
 
 export const Test = () => {
   return (
-    <>
+    <div className="test">
       <h2>Тест</h2>
-    </>
+      <p>Пниаыф арфлыа да ф аф аф а афа</p>
+    </div>
   );
 };
